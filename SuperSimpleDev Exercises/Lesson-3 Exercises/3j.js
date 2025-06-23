8e7805cdecceaefc66061baef31dabe0a0dfea45
Sam-2610 <satyamsagar827@gmail.com>
2025-06-23 11:38:05 +0530

    #

--- SuperSimpleDev Exercises/Lesson-3 Exercises/3j.js
+++ SuperSimpleDev Exercises/Lesson-3 Exercises/3j.js
@@ -5,4 +5,4 @@ let coffee = 5.99;
 let bagel = 2.95;
 let total = (coffee + bagel) * 100; 
 let message = `Total cost: $${(total / 100).toFixed(2)}`;
-alert(message  + '\nThank You, Come again!'); 
+alert(`${message}\nThank You, Come again!`); 
