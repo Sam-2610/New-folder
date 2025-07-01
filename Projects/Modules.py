c6648df0ab1b415c6a186a238df50efd02391b6c
Sam-2610 <satyamsagar827@gmail.com>
2025-07-01 19:35:20 +0530

    Modules

---
+++ Projects/Modules.py
@@ -0,0 +1,36 @@
+import math
+import statistics as s
+import random 
+
+
+x = 22.34
+y = 12.34
+z = -11.23
+l =[10,20,30,40,50]
+
+# Math Module
+
+print(max(x,y,z)) 
+print(min(x,y,z))
+print(abs(x))
+print(pow(x,y))
+print(math.sqrt(16))
+print(math.ceil(x))
+print(math.floor(y))
+print(math.e)
+print(math.pi)
+print(math.inf)
+print(math.tau)
+
+# Statistics Module
+
+print(s.mean(l))
+print(s.median(l))
+print(s.mode(l))
+
+# Random Module
+
+print(random.random())
+print(random.randint(1,5))
+print(random.randrange(1,5))
+
