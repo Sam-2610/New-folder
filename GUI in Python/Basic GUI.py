98c3da3a09c0124c30388e6aad794f46c1ace485
Sam-2610 <satyamsagar827@gmail.com>
2025-07-25 08:44:04 +0530

    #

--- GUI in Python/Basic GUI.py
+++
@@ -1,3 +0,0 @@
-import tkinter         # Imports the tkinter module
-m = tkinter.Tk()       # Creates the main window (also called root window)
-m.mainloop()           # Starts the Tkinter event loop, displaying the window
--- GUI in Python/Button.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-win = Tk()
-win.title("Buttons in GUI")
-
-Button(win,text="STOP",width= 25,command= win.destroy).pack()
-
-mainloop()
