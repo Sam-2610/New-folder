0ebba096b258e60663ad9625424e3a2b5affc79c
Satyam Sagar <satyamsagar827@gmail.com>
2026-03-13 21:51:44 +0530

    Converting into Functions

--- Python Practice-2025/2.py
+++ Python Practice-2025/2.py
@@ -1,2 +1,13 @@
-# Print Hello World
-print("Hello World")
+""" Print Hello World """
+# Exception Handling
+""" try:
+    print("Hello World")
+except Exception as e:
+    print(e) """
+
+# Function 
+
+def message():
+    return "Hello World"
+
+print(message())
--- Python Practice-2025/8.py
+++ Python Practice-2025/8.py
@@ -1,5 +1,8 @@
-# Check if a number is positive,negative or zero
-try:
+"""  Check if a number is positive,negative or zero """
+
+# Exception Handling 
+
+""" try:
     x = int(input("Enter a Number: "))
     if x < 0:
         print(f"The Number {x} is Negative!")
@@ -8,4 +11,17 @@ try:
     else:
         print(f"The Number is Zero!")
 except Exception as e:
-    print("Please Enter a Valid Integer!",e)
+    print("Please Enter a Valid Integer!",e) """
+
+# Function 
+
+def check(x):
+    if x < 0:
+        print(f"The Number {x} is Negative!")
+    elif x > 0:
+        print(f"The Number {x} is Positive!")
+    else:
+        print(f"The Number is Zero!")
+
+x = int(input("Enter a Number : "))
+print(check(x))
