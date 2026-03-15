4ba2e612fed95bf98e4bb4fe9b4d5744f04205b0
Satyam Sagar <satyamsagar827@gmail.com>
2026-03-15 10:34:37 +0530

    Update product prices for accuracy and improve output formatting

--- Python Practice-2025/51.py
+++ Python Practice-2025/51.py
@@ -14,13 +14,17 @@ class Products:
     def calculate_discount(price, discount):
         print(f"Discounted Price = {price - (price * discount/100)}")
 
-p1 = Products("Phone",10000)
+p1 = Products("Phone",6999)
 print(f"The Name of the Product is {p1.name} and the Price is {p1.price}")
 p1.calculate_discount(p1.price,10)
 #------------------------------------------------------------------------------#
-p2 = Products("TV",1000)
+p2 = Products("TV",3599)
 print(f"The Name of the Product is {p2.name} and the Price is {p2.price}")
 p2.calculate_discount(p2.price,12)
+#------------------------------------------------------------------------------#
+p3 = Products("Oven",5499)
+print(f"The Name of the Product is {p3.name} and the Price is {p3.price}")
+p3.calculate_discount(p3.price,14)
 #------------------------------------------------------------------------------#
 print(f"The Total Number of Products : {Products.count}")
 
