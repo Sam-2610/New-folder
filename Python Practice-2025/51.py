b16fc6565e2fd2f4c10f91860c210ab57ce28a70
Satyam Sagar <satyamsagar827@gmail.com>
2026-03-15 10:08:54 +0530

    Implement product management system with discount calculation

---
+++ Python Practice-2025/51.py
@@ -0,0 +1,26 @@
+""" Design & create an online store for Products (name, price).
+Track total products being created.
+Create a static method to calculate discount on each product based on a % parameter. """
+
+class Products:
+    count = 0
+    
+    def __init__(self,name,price):
+        self.name = name
+        self.price = price
+        Products.count = Products.count + 1
+
+    @staticmethod
+    def calculate_discount(price, discount):
+        print(f"Discounted Price = {price - (price * discount/100)}")
+
+p1 = Products("Phone",10000)
+print(f"The Name of the Product is {p1.name} and the Price is {p1.price}")
+p1.calculate_discount(p1.price,10)
+#-------------------------------------------------------------------------------
+p2 = Products("TV",1000)
+print(f"The Name of the Product is {p2.name} and the Price is {p2.price}")
+p2.calculate_discount(p2.price,12)
+#-------------------------------------------------------------------------------
+print(f"The Total Number of Products : {Products.count}")
+
